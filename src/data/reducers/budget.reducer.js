065e60c614dfa94efabed0a7e2a04171cb8e28e7
g-krzysztof <hello@krzysztofg.pl>
2021-05-30 09:17:22 +0200

    add redux and basic store file structure

---
+++ src/data/reducers/budget.reducer.js
@@ -0,0 +1,46 @@
+import {
+  BUDGET_GET,
+  BUDGET_GET_REQUEST,
+  BUDGET_GET_SUCCESS,
+  BUDGET_GET_FAILURE,
+  LOADING_STATES
+} from 'data/constants'
+
+const initialState = {
+  budget: {},
+  budgetedCategories: [],
+  loadingState: {}
+}
+
+function budget (state = initialState, action) {
+  const newLoadingState = { ...state.loadingState }
+
+  switch (action.type) {
+    case BUDGET_GET_REQUEST:
+      return {
+        ...state,
+        loadingState: {
+          ...state.loadingState,
+          [action.type]: LOADING_STATES.LOADING
+        }
+      }
+    case BUDGET_GET_FAILURE:
+      delete newLoadingState.BUDGET_GET_REQUEST
+      return {
+        ...state,
+        budget: {},
+        loadingState: newLoadingState
+      }
+    case BUDGET_GET_SUCCESS:
+      delete newLoadingState.BUDGET_GET_REQUEST
+      return {
+        ...state,
+        budget: action.payload,
+        loadingState: newLoadingState
+      }
+    default:
+      return state
+  }
+}
+
+export default budget
